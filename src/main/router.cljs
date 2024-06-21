d119a83940d6bf1dae402dab48353f6d28f51fbe
Alonso Suarez <alonso.suarez@cloudloto.com>
2024-06-21 13:41:50 -0700

    testing e2e router

--- src/main/router.cljs
+++ src/main/router.cljs
@@ -5,7 +5,6 @@
             [reitit.ring.malli]
             [malli.core :as m]
             [reitit.swagger :as swagger]
-            [reitit.swagger-ui :as swagger-ui]
             [reitit.ring.coercion :as coercion]
             [reitit.dev.pretty :as pretty]
             [reitit.coercion.malli :as rcm]
@@ -15,9 +14,9 @@
                ;       [spec-tools.spell :as spell]
             [malli.util :as mu]
             [malli.error :as me]
+            ["swagger-ui-dist" :as swagger-ui]
             [clojure.string :as str]
             [malli.transform :as mt]
-            ["import-modules" :as import-modules]
             [reitit.spec :as rs]))
 
 (def app-header
@@ -36,42 +35,76 @@
                :request-method :post
                :headers  {:cookie {:token "1"}}}))
 
+(defn get-content-type [filename]
+  (let [extension (last (str/split filename #"\."))]
+    (case extension
+      "txt" "text/plain"
+      "html" "text/html"
+      "css" "text/css"
+      "js" "application/javascript"
+      "json" "application/json"
+      "png" "image/png"
+      "jpg" "image/jpeg"
+      "application/octet-stream")))
+(comment
+  (get-content-type "swagger-initializer.js"))
+(def swagger-path (.absolutePath swagger-ui))
+(defn remove-petstore-url [string-buffer]
+  (str/replace string-buffer "https://petstore.swagger.io/v2" ""))
 
-(defn app []
+(comment
+  (remove-petstore-url ""))
+
+(def app
   (ring/ring-handler
-   (ring/router ["/plus/{z}"
-                 {:post {:summary "plus with malli query parameters"
-                         :parameters {:path [:map
-                                             [:z int?]]
-                                      :query [:map
-                                              [:x
-                                               {:title "X parameter"
-                                                :description "Description for X parameter"
-                                                :json-schema/default 42}
-                                               int?]]
-                                      :body [:map
-                                             [:y int?]]
-                                      :header [:map
-                                               [:cookie
-                                                [:map
-                                                 [:t :int]]]]}
-                         :responses {200 {:body [:map [:total int?]]}}
-                         :handler (fn [data]
-                                    (let [{:keys [parameters header-params]} data
-                                          {{:keys [x]} :query
-                                           {:keys [y]} :body
-                                           {:keys [z]} :path
-                                           {{:keys [TOKEN t]} :cookie} :header} parameters]
-                                      (println "==")
-                                      (js/console.log (clj->js parameters))
+   (ring/router [["" {:no-doc true}
+                  ["/swagger.json"
+                   {:get (swagger/create-swagger-handler)}]
+                  ["/api-docs/{*path}"
+                   {:get {:handler
+                          (fn [{{:keys [path]} :path-params}]
+                            (let [file (str swagger-path "/" path)
+                                  content (-> (.readFileSync fs file {"encoding" 'utf8' "flag" 'r'})
+                                              (str))
+                                  parsed-content (cond
+                                                   (= path "swagger-initializer.js") (remove-petstore-url content)
+                                                   :else content)]
+                              {:status 200
+                               :body parsed-content
+                               :headers {:content-type (get-content-type path)}}))}}]]
+                 ["/plus/{z}"
+                  {:post {:summary "plus with malli query parameters"
+                          :parameters {:path [:map
+                                              [:z int?]]
+                                       :query [:map
+                                               [:x
+                                                {:title "X parameter"
+                                                 :description "Description for X parameter"
+                                                 :json-schema/default 42}
+                                                int?]]
+                                       :body [:map
+                                              [:y int?]]
+                                       :header [:map
+                                                [:cookie
+                                                 [:map
+                                                  [:t :int]]]]}
+                          :responses {200 {:body [:map [:total int?]]}}
+                          :handler (fn [data]
+                                     (let [{:keys [parameters header-params]} data
+                                           {{:keys [x]} :query
+                                            {:keys [y]} :body
+                                            {:keys [z]} :path
+                                            {{:keys [TOKEN t]} :cookie} :header} parameters]
+                                       (println "==")
+                                       (js/console.log (clj->js parameters))
 
-                                      {:status 200
-                                       :body {:total (+ x y z t)}}))}}]
+                                       {:status 200
+                                        :body {:total (+ x y z t)}}))}}]]
                 {:data {:coercion rcm/coercion
                         :middleware [coercion/coerce-exceptions-middleware
                                      coercion/coerce-request-middleware
                                      coercion/coerce-response-middleware]}})
-   (constantly {:status 404, :body ""})))
+   (ring/create-default-handler)))
 
 
 (comment
@@ -92,7 +125,6 @@
   [cookie-str]
   (->> (str/split cookie-str #"; ")
        (map #(str/split % #"="))
-      ;;  (map (fn [[k v]] [(keyword (str/lower-case k)) v]))
        (into {})))
 
 (comment (cookies-str->clj "TOKEN=123; SECRET=432"))
@@ -107,9 +139,13 @@
     (empty? data) {}
     :else (str-json->clj data)))
 
-(defn read-json [path]
+(defn read-json->clj [path]
   (-> (.readFileSync fs path {"encoding" 'utf8' "flag" 'r'})
       (str-json->clj)))
+
+(defn read-json->js [path]
+  (-> (.readFileSync fs path {"encoding" 'utf8' "flag" 'r'})
+      (js/JSON.parse)))
 
 (def Event
   [:map
@@ -123,24 +159,20 @@
                               [:path :string]]]]]])
 
 
-(def event (read-json "./event.json"))
+(def event (read-json->js "./event.json"))
 
-(defn read-event [path]
-  (let [event (read-json path)]
-    (try
-      (m/coerce Event event strict-json-transformer)
-      (catch js/Error e
-        (-> e ex-data :data :explain me/humanize)))))
-
-(comment
+(defn coerce-event [event]
   (try
-    (read-event "./event.json")
+    (-> event
+        (js->clj)
+        (#(m/coerce Event % strict-json-transformer)))
     (catch js/Error e
       (-> e ex-data :data :explain me/humanize))))
 
-(defn handler [path-routes]
-  (let [routes-json (read-json path-routes)
-        event (read-event "./event.json")
+(defn handler [path-routes, js-event]
+  (let [routes-json (read-json->clj path-routes)
+        ;; event (read-event "./event.json")
+        event (coerce-event js-event)
         {:keys [body query-string-parameters headers request-context]} event
         {:keys [http]} request-context
         {:keys [method path]} http
@@ -149,47 +181,35 @@
                  :request-method method
                  :body-params body
                  :headers headers}
-        response ((app) request)]
+        response (app request)]
+    (js/console.log js-event)
     (println path query-string-parameters body headers method)
     (println response)
-    (println (get-in response [:body :humanized]))
-    response))
 
-
-(comment
-  (handler "./event.json"))
-
-(defn fn-to-call-on-load []
-  (println "plus loaded"))
-
-(def plus (str (js/process.cwd) "/plus.mjs"))
+    (-> response
+        (assoc :statusCode (:status response))
+        (dissoc :status)
+        (clj->js))))
 
 (comment
-  (do (require '["./modules/plus.js" :as plus] :reload)
-      (print plus))
-  
-  (js/console.log (->
-                   (.readFileSync fs "plus.js" {"encoding" 'utf8' "flag" 'r'})
-                   (str)
-                   (js/eval) 
-                   (.handler )))
-  (js/shadow.js.jsRequire "./modules/plus")
-  (str js/__filename)
-  (js/process.cwd)
-  (-> (import-modules "modules")
-      (js->clj :keywordize-keys true)
-      (:plus)
-      (:handler)
-      (apply "hello")
-      (.then print)))
+  (handler "./event.json" {})
+  ((app) {:request-method :get :uri "/swagger.json"})
+  ((app) {:request-method :get :uri "/api-docs/swagger-initializer.js"})
+  ((app) {:request-method :get :uri "/nothing"}))
+
+(comment
+  (-> (js/require "./plus.js")
+      (js->clj)
+      (get "handler")
+      (apply "hola")
+      (.then js/console.log)))
 (defn ^:dev/before-load stop [])
   ;; (println "============= stop =================")
 
 (defn ^:dev/after-load start []
   (println "============= restart =================")
-  (handler "./event.json")
+  (-> (handler "./event.json" (read-json->js "./event.json"))
+      (js/console.log))
   ;; (require '[ :as plus] :reload)
   )
-
-  ;; (js/console.log (read-json "./event.json"))
   
