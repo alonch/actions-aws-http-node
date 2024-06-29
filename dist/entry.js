e4809b70e4219e6564d73df9ea547f14147148c8
Alonso Suarez <alonchb@gmail.com>
2024-06-28 23:12:07 -0700

    log event and response

--- dist/entry.js
+++ dist/entry.js
@@ -1,9 +1,9 @@
 const router = require("./actions-aws-http-node.js")
 
 exports.handler = function (event, context, callback) {
-    console.log(event)
+    console.log(JSON.stringify(event))
     const wrapper = (response) => { 
-        console.log(response)
+        console.log(JSON.stringify(response))
         callback(null, response) }
     router.handler("./event.json", event, wrapper)
 };
