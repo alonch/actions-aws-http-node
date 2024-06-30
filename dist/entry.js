8246d732b545770534c92121f10b0a2c01351d59
Alonso Suarez <alonchb@gmail.com>
2024-06-29 23:30:53 -0700

    refactor and use async

--- dist/entry.js
+++ dist/entry.js
@@ -1,6 +1,6 @@
 const router = require("./actions-aws-http-node.js")
 
-exports.handler = async function (event, context, callback) {
+exports.handler = async (event, context, callback) => {
     console.log(JSON.stringify(event))
     const response = await new Promise((resolve, reject) => {
         router.handler("./event.json", event, resolve)
